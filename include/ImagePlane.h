51cb062a8a89ec0f8b01b970245ef6f244a47f81
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-06-08 14:21:53 +0300

    Comment out image stuff - work on geometry and B-fields only

--- include/ImagePlane.h
+++ include/ImagePlane.h
@@ -4,30 +4,30 @@
 
 #ifndef JETSHOW_IMAGEPLANE_H
 #define JETSHOW_IMAGEPLANE_H
-
-#include <array>
-#include <Eigen/Eigen>
-#include "Image.h"
-#include "Ray.h"
-
-using std::array;
-using Eigen::Vector3d;
-
-
-class ImagePlane {
-public:
-    ImagePlane(array<int,2> image_size, array<double,2> pixel_size,
-               array<double,2> pixel_scales, double los_angle);
-    array<Pixel,image_.num_of_pixels_> getPixels();
-
-private:
-    Image image_;
-    double los_angle_;
-    Vector3d direction_;
-    array<Ray,image_.num_of_pixels_> rays_;
-
-
-};
-
+//
+//#include <array>
+//#include <Eigen/Eigen>
+//#include "Image.h"
+//#include "Ray.h"
+//
+//using std::array;
+//using Eigen::Vector3d;
+//
+//
+//class ImagePlane {
+//public:
+//    ImagePlane(array<int,2> image_size, array<double,2> pixel_size,
+//               array<double,2> pixel_scales, double los_angle);
+//    array<Pixel,image_.num_of_pixels_> getPixels();
+//
+//private:
+//    Image image_;
+//    double los_angle_;
+//    Vector3d direction_;
+//    array<Ray,image_.num_of_pixels_> rays_;
+//
+//
+//};
+//
 
 #endif //JETSHOW_IMAGEPLANE_H
--- src/Image.cpp
+++ src/Image.cpp
@@ -2,21 +2,21 @@
 // Created by ilya on 6/5/17.
 //
 
-#include <array>
-#include <string>
-#include "Image.h"
-
-using std::array
-using std::string
-
-
-array<array<double, image_size_[0]>, image_size_[1]> Image::get_image(string value) {
-    return array<array<double, image_size_[0]>, image_size_[1]>();
-}
-
-Image::Image(array<int, 2> image_size, array<double, 2> pixel_size,
-             array<double, 2> pixel_scales):
-        image_size_(image_size), pixel_size_(pixel_size),
-        pixel_scales_(pixel_scales),
-        num_of_pixels_(image_size_[0] * image_size_[1]),
-        pixels_() {}
+//#include <array>
+//#include <string>
+//#include "Image.h"
+//
+//using std::array
+//using std::string
+//
+//
+//array<array<double, image_size_[0]>, image_size_[1]> Image::get_image(string value) {
+//    return array<array<double, image_size_[0]>, image_size_[1]>();
+//}
+//
+//Image::Image(array<int, 2> image_size, array<double, 2> pixel_size,
+//             array<double, 2> pixel_scales):
+//        image_size_(image_size), pixel_size_(pixel_size),
+//        pixel_scales_(pixel_scales),
+//        num_of_pixels_(image_size_[0] * image_size_[1]),
+//        pixels_() {}
