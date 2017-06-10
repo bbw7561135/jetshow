6f1e722299a137953b49a85c91c0dea615f578a7
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-06-10 21:19:43 +0300

    note on possible implementations

--- include/VField.h
+++ include/VField.h
@@ -9,7 +9,12 @@
 
 using Eigen::Vector3d;
 
-
+// TODO: What is more efficient?
+// 1. Returning type Vector3d. Thus returning a value. Copying can be avoided
+// via RVO?
+// 2. Returning a reference. I need lvalue expression inside member function to
+// reference it. Thus creating and possibly copying. Is it optimized?
+// 3. Using move semantic?
 class VField {
 public:
     virtual Vector3d v(const Vector3d& point) const = 0;
