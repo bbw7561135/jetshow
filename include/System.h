51357e0e7b5efbdcc3bbe8aa9795f60e0a6bd402
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-06-14 14:38:43 +0300

    Using class for ODE

--- include/System.h
+++ include/System.h
@@ -1,8 +1,45 @@
 #ifndef JETSHOW_SYSTEMS_H
 #define JETSHOW_SYSTEMS_H
 
-class Systems {
+#include <iostream>
+#include "Jet.h"
 
+
+class System {
+ public:
+  // Default ctor
+  // System();
+  System(Jet* newjet, Vector3d &newpoint_in, Vector3d &newray_direction,
+         double newnu);
+
+  // This is what differs
+  virtual void operator() (const double &x, double &dxdt, const double t) = 0;
+
+  void setPointIn(Vector3d &newpoint_in) {
+    point_in = newpoint_in;
+  }
+  void setRayDirection(Vector3d &newray_direction) {
+    ray_direction = newray_direction;
+  }
+
+ protected:
+  Jet* jet;
+  Vector3d point_in;
+  Vector3d ray_direction;
+  double nu;
 };
+
+
+class Tau : public System {
+ public:
+  Tau(Jet* newjet, Vector3d &newpoint_in, Vector3d &newray_direction,
+      double newnu) : System(newjet, newpoint_in, newray_direction, newnu) {};
+
+  void operator() (const double &x, double &dxdt, const double t) override;
+};
+
+
+// This debug-print integrands and ``time`` variable
+void write_cout(const double &x, const double t);
 
 #endif //JETSHOW_SYSTEMS_H
--- src/System.cpp
+++ src/System.cpp
@@ -1,5 +1,23 @@
-//
-// Created by ilya on 6/13/17.
-//
+#include "System.h"
 
-#include "Systems.h"
+
+System::System(Jet *newjet,
+               Vector3d &newpoint_in,
+               Vector3d &newray_direction,
+               double newnu) {
+  jet = newjet;
+  point_in = newpoint_in;
+  ray_direction = newray_direction;
+  nu = newnu;
+}
+
+
+void Tau::operator()(const double &x, double &dxdt, const double t) {
+  Vector3d point = point_in + t * ray_direction;
+  //std::cout << "calculating k_I at point " << point << std::endl;
+  dxdt = jet->getKI(point, ray_direction, nu);
+}
+
+void write_cout(const double &x, const double t) {
+  std::cout << t << '\t' << x << std::endl;
+}
