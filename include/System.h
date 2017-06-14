eb2c2cd3346c17bff507254c49449f2a54a6e74b
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-06-14 14:38:07 +0300

    C14 with make_unique

---
+++ include/System.h
@@ -0,0 +1,8 @@
+#ifndef JETSHOW_SYSTEMS_H
+#define JETSHOW_SYSTEMS_H
+
+class Systems {
+
+};
+
+#endif //JETSHOW_SYSTEMS_H
---
+++ src/System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ilya on 6/13/17.
+//
+
+#include "Systems.h"
