ac91e1d83eb0ac941b0674703d2f0ff59595308c
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-06-30 17:26:49 +0300

    update how to make plots and substitute real data with BK

--- example_simple.py
+++ example_simple.py
@@ -1,41 +1,27 @@
-imsize = (200,200)
-mas_in_pix = 0.02
+import os
+os.chdir('/home/ilya/github/vlbi_errors/vlbi_errors')
+from components import ImageComponent
+from uv_data import UVData
+from model import Model
+from utils import mas_to_rad
+uvdata = UVData('0235+164.x.2006_06_15.uvf')
+imsize = (500,500)
+mas_in_pix = 0.002
 y, z = np.meshgrid(np.arange(imsize[0]), np.arange(imsize[1]))
 y = y - imsize[0] / 2. + 0.5
 z = z - imsize[0] / 2. + 0.5
 y_mas = y*mas_in_pix
 z_mas = z*mas_in_pix
-from utils import mas_to_rad
 y_rad = mas_to_rad * y_mas
 z_rad = mas_to_rad * z_mas
-yy, xx = np.where(image != 0)
-x_ = y_rad[0,:]; y_ = z_rad[:,0]
-xx = x_[xx]
-yy = y_[yy]
-uvdata = UVData('0235+164.x.2006_06_15.uvf')
-u = uvdata.uv[:, 0]
-v = uvdata.uv[:, 1]
-
-visibilities = list()
-for u0, v0 in zip(u, v):
-    visibility = (image.compressed() * np.exp(-2.0 * math.pi * 1j *
-                                              (u0 * xx + v0 * yy))).sum()
-    visibilities.append(visibility)
-
-amps = [np.sqrt(v*v.conjugate()).real for v in visibilities]
-phases = [angle(v) for v in visibilities]
-uv_rad = list()
-for u_, v_ in zip(u,v):
-    uv_rad.append(np.sqrt(u_*u_+v_*v_))
-
-plot(uv_rad, amps, '.k')
-plot(uv_rad, phases, '.k')
-
-
+image_i = np.loadtxt('/home/ilya/github/bck/jetshow/cmake-build-debug/map_i.txt')
+image_tau = np.loadtxt('/home/ilya/github/bck/jetshow/cmake-build-debug/map_tau.txt')
+matshow(image_i);colorbar()
+contour(log10(image_tau), [-1, -0.5, 0, 0.5, 1, 2], cmap='tab10');colorbar()
 icomp = ImageComponent(image_i, y_rad[0,:], z_rad[:,0])
 noise = uvdata.noise(use_V=True)
-from model import Model
 model = Model(stokes='I')
 model.add_component(icomp)
 uvdata.substitute([model])
 uvdata.uvplot()
+
