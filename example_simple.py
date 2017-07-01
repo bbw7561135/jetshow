970f116de778ebff8ef21c8946c0fb4557cff70a
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-07-01 16:33:17 +0300

    update example

--- example_simple.py
+++ example_simple.py
@@ -23,5 +23,31 @@ noise = uvdata.noise(use_V=True)
 model = Model(stokes='I')
 model.add_component(icomp)
 uvdata.substitute([model])
+uvdata.noise_add(noise)
+uvdata.save('/home/ilya/github/bck/jetshow/bk.fits')
 uvdata.uvplot()
+
+
+from spydiff import modelfit_difmap, clean_difmap, import_difmap_model
+from image import plot as iplot
+from image import find_bbox
+from from_fits import create_clean_image_from_fits_file
+from image_ops import rms_image
+
+path_to_script = '/home/ilya/github/vlbi_errors/difmap/final_clean_nw'
+data_dir = '/home/ilya/github/bck/jetshow/'
+clean_difmap('bk.fits', 'bk_cc.fits', 'I', (1024, 0.1), path=data_dir,
+             path_to_script=path_to_script, show_difmap_output=True,
+             outpath=data_dir)
+modelfit_difmap('bk.fits', 'initial.mdl', 'bk.mdl', niter=100, path=data_dir,
+                mdl_path=data_dir, out_path=data_dir)
+             
+ccimage = create_clean_image_from_fits_file(os.path.join(data_dir, 'bk_cc.fits'))
+beam = ccimage.beam
+rms = rms_image(ccimage)
+blc, trc = find_bbox(ccimage.image, rms, 10)
+comps = import_difmap_model('bk.mdl', data_dir)
+core = comps[0]
+iplot(ccimage.image, x=ccimage.x, y=ccimage.y, min_abs_level=3*rms, beam=beam,
+      show_beam=True, blc=blc, trc=trc, core=core.p)
 
