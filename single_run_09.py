a2c8f840c608ba6968f760b7ec320c2f11b22e57
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-12-02 18:46:19 +0300

    [skip ci] update

--- single_run_09.py
+++ single_run_09.py
@@ -8,13 +8,13 @@ cfg_file = os.path.join(main_dir, 'config.json')
 los = 0.1
 b = 1.0
 n = 5000.0
-freq = 4.8
+freq = 22.4
 
 update_dict = {"jet": {"bfield": {"parameters": {"b_1": b}},
                        "nfield": {"parameters": {"n_1": n}}},
                "observation": {"los_angle": los,
                                "frequency_ghz": freq},
-               "image": {"pixel_size_mas": 0.01, "number_of_pixels": 400}}
+               "image": {"pixel_size_mas": 0.00253, "number_of_pixels": 756}}
 update_config(cfg_file, update_dict)
 
 simulation_params = run_simulations(cfg_file, path_to_executable)
