83fffc88cbf2edbdc4f9a58a989c8921c79d4ae5
Ilya Pashchenko <in4pashchenko@gmail.com>
2018-01-07 13:58:46 +0300

    commenting out unused coefficients

--- include/Jet.h
+++ include/Jet.h
@@ -17,7 +17,7 @@ public:
 		    NField* nField);
 
     // Vector of the magnetic field in the lab frame at point ``point``.
-    const Vector3d getB(const Vector3d& point);
+//    const Vector3d getB(const Vector3d& point);
 
     // Vector of the bulk motion speed in the lab frame at point ``point``.
     const Vector3d getV(const Vector3d& point);
@@ -30,29 +30,29 @@ public:
     // frame. ``n`` is unit LOS vector in the observer frame.
     double getKI(Vector3d &point, Vector3d &n_los, double nu);
 
-		double getKQ(Vector3d &point, Vector3d &n_los, double nu);
-
-		double getKU(Vector3d &point, Vector3d &n_los, double nu);
-
-		// TODO: For e+ must be "+" sign
-		double getKV(Vector3d &point, Vector3d &n_los, double nu);
-
-		double getKF(Vector3d &point, Vector3d &n_los, double nu);
-
-		double getKC(Vector3d &point, Vector3d &n_los, double nu);
-
-		double gethQ(Vector3d &point, Vector3d &n_los, double nu);
+//		double getKQ(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		double getKU(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		// TODO: For e+ must be "+" sign
+//		double getKV(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		double getKF(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		double getKC(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		double gethQ(Vector3d &point, Vector3d &n_los, double nu);
 
     // Emission coefficient in ``point`` of the jet in the observer (lab)
     // frame. ``n`` is unit LOS vector in the observer frame.
     double getEtaI(Vector3d &point, Vector3d &n_los, double nu);
 
-		double getEtaQ(Vector3d &point, Vector3d &n_los, double nu);
-
-		double getEtaU(Vector3d &point, Vector3d &n_los, double nu);
-
-		// TODO: For e+ must be "+" sign
-		double getEtaV(Vector3d &point, Vector3d &n_los, double nu);
+//		double getEtaQ(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		double getEtaU(Vector3d &point, Vector3d &n_los, double nu);
+//
+//		// TODO: For e+ must be "+" sign
+//		double getEtaV(Vector3d &point, Vector3d &n_los, double nu);
 
     std::list<Intersection> hit(Ray& ray);
 
--- src/Jet.cpp
+++ src/Jet.cpp
@@ -21,9 +21,12 @@ double Jet::getKI(Vector3d &point, Vector3d &n_los, double nu) {
     // n_los_prime = f(n_los, v)
     // nu_prime = f(nu, n_los, v) = nu/getD
     // n_prime = f(n, v) = n/Gamma
-    Vector3d b = getB(point);
+
+	  // This conflicts with scalar valued random B-field
+//	  Vector3d b = getB(point);
     Vector3d v = getV(point);
     auto D = getD(n_los, v);
+//	  std::cout << "in K_I D = " << D << std::endl;
     auto gamma = getG(v);
     double n = getN(point);
 //    auto b_prime = getBprime(b, v);
@@ -36,101 +39,107 @@ double Jet::getKI(Vector3d &point, Vector3d &n_los, double nu) {
     return k_i_prime/D;
 }
 
-double Jet::getKQ(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto k_q_prime = k_q(b_prime, n_los_prime, nu_prime, n_prime);
-	return k_q_prime/D;
-}
-
-double Jet::getKU(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto k_u_prime = k_u(b_prime, n_los_prime, nu_prime, n_prime);
-	return k_u_prime/D;
-}
-
-double Jet::getKV(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto k_v_prime = k_v(b_prime, n_los_prime, nu_prime, n_prime);
-	return k_v_prime/D;
-}
-
-double Jet::getKF(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto k_F_prime = k_F_r(b_prime, n_los_prime, nu_prime, n_prime);
-	return k_F_prime/D;
-}
-
-double Jet::getKC(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto k_C_prime = k_C_r(b_prime, n_los_prime, nu_prime, n_prime);
-	return k_C_prime/D;
-}
-
-double Jet::gethQ(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto h_Q_prime = h_Q(b_prime, n_los_prime, nu_prime, n_prime);
-	return h_Q_prime/D;
-}
+//double Jet::getKQ(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto k_q_prime = k_q(b_prime, n_los_prime, nu_prime, n_prime);
+//	return k_q_prime/D;
+//}
+//
+//double Jet::getKU(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto k_u_prime = k_u(b_prime, n_los_prime, nu_prime, n_prime);
+//	return k_u_prime/D;
+//}
+//
+//double Jet::getKV(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto k_v_prime = k_v(b_prime, n_los_prime, nu_prime, n_prime);
+//	return k_v_prime/D;
+//}
+//
+//double Jet::getKF(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto k_F_prime = k_F_r(b_prime, n_los_prime, nu_prime, n_prime);
+//	return k_F_prime/D;
+//}
+//
+//double Jet::getKC(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto k_C_prime = k_C_r(b_prime, n_los_prime, nu_prime, n_prime);
+//	return k_C_prime/D;
+//}
+//
+//double Jet::gethQ(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto h_Q_prime = h_Q(b_prime, n_los_prime, nu_prime, n_prime);
+//	return h_Q_prime/D;
+//}
 
 
 // This is eta_i in lab frame that could be integrated along LOS.
@@ -143,7 +152,9 @@ double Jet::getEtaI(Vector3d &point, Vector3d &n_los, double nu) {
     // n_los_prime = f(n_los, v)
     // nu_prime = f(nu, n_los, v) = nu/getD
     // n_prime = f(n, v) = n/Gamma
-    Vector3d b = getB(point);
+
+	// This conflicts with scalar valued random B-field
+//    Vector3d b = getB(point);
     Vector3d v = getV(point);
     auto D = getD(n_los, v);
     auto gamma = getG(v);
@@ -158,58 +169,61 @@ double Jet::getEtaI(Vector3d &point, Vector3d &n_los, double nu) {
     return eta_i_prime*D*D;
 }
 
-double Jet::getEtaQ(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto eta_q_prime = eta_q(b_prime, n_los_prime, nu_prime, n_prime);
-	return eta_q_prime*D*D;
-}
+//double Jet::getEtaQ(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto eta_q_prime = eta_q(b_prime, n_los_prime, nu_prime, n_prime);
+//	return eta_q_prime*D*D;
+//}
+//
+//double Jet::getEtaU(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto eta_u_prime = eta_u(b_prime, n_los_prime, nu_prime, n_prime);
+//	return eta_u_prime*D*D;
+//}
+//
+//double Jet::getEtaV(Vector3d &point, Vector3d &n_los, double nu) {
+//	// This conflicts with scalar valued random B-field
+////	Vector3d b = getB(point);
+//	Vector3d v = getV(point);
+//	auto D = getD(n_los, v);
+//	auto gamma = getG(v);
+//	double n = getN(point);
+////	auto b_prime = getBprime(b, v);
+//	// This means that we are using B-fields specification in the plasma frame
+//	auto b_prime = bfield_->bf(point);
+//	auto n_los_prime = get_n_los_prime(n_los, v);
+//	auto nu_prime = nu/D;
+//	auto n_prime = n/gamma;
+//	auto eta_v_prime = eta_v(b_prime, n_los_prime, nu_prime, n_prime);
+//	return eta_v_prime*D*D;
+//}
 
-double Jet::getEtaU(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto eta_u_prime = eta_u(b_prime, n_los_prime, nu_prime, n_prime);
-	return eta_u_prime*D*D;
-}
 
-double Jet::getEtaV(Vector3d &point, Vector3d &n_los, double nu) {
-	Vector3d b = getB(point);
-	Vector3d v = getV(point);
-	auto D = getD(n_los, v);
-	auto gamma = getG(v);
-	double n = getN(point);
-//	auto b_prime = getBprime(b, v);
-	// This means that we are using B-fields specification in the plasma frame
-	auto b_prime = bfield_->bf(point);
-	auto n_los_prime = get_n_los_prime(n_los, v);
-	auto nu_prime = nu/D;
-	auto n_prime = n/gamma;
-	auto eta_v_prime = eta_v(b_prime, n_los_prime, nu_prime, n_prime);
-	return eta_v_prime*D*D;
-}
-
-
-const Vector3d Jet::getB(const Vector3d &point) {
-    return bfield_->bf(point);
-}
+//const Vector3d Jet::getB(const Vector3d &point) {
+//    return bfield_->bf(point);
+//}
 
 const Vector3d Jet::getV(const Vector3d &point) {
     return vfield_->v(point);
@@ -223,5 +237,6 @@ const double Jet::getN(const Vector3d &point) {
 std::list<Intersection> Jet::hit(Ray &ray) {
   return geometry_->hit(ray);
 }
+
 
 
