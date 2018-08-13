f145af5de738a9d3d8dde5f1c7b603e9903850f5
Ilya Pashchenko <in4pashchenko@gmail.com>
2018-08-14 02:08:58 +0300

    Density field made of two power laws

--- src/NField.cpp
+++ src/NField.cpp
@@ -10,6 +10,22 @@ double BKNField::n(const Vector3d &point) const {
     return n_0_*pow(r/pc, -n_n_);
 }
 
+
+CompositeBKNField::CompositeBKNField(double n_0, double n_n_inner, double n_n_outer, double z0) :
+    inner_field_(n_0, n_n_inner), outer_field_(n_0, n_n_outer), z0_(z0) {};
+
+
+double CompositeBKNField::n(const Vector3d &point) const {
+    double z = point[2];
+    if (z > z0_) {
+        return outer_field_.n(point);
+    }
+    else {
+        return inner_field_.n(point);
+    }
+}
+
+
 ConstNField::ConstNField(double n) : n_(n) {};
 
 double ConstNField::n(const Vector3d &point) const {
