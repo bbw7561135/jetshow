e078dadcbb981ff1cbb0a9004c1a412ae0ac5c40
Ilya Pashchenko <in4pashchenko@gmail.com>
2018-08-14 16:52:52 +0300

    Fix inersection for parabolic geometry

--- src/Parabaloid.cpp
+++ src/Parabaloid.cpp
@@ -8,7 +8,7 @@
 #include "Geometry.h"
 
 
-Parabaloid::Parabaloid(const Vector3d &origin, const Vector3d &direction, const double &r0) {
+Parabaloid::Parabaloid(const Vector3d &origin, const Vector3d &direction, const double &r0, const double &big_scale) {
     origin_ = origin;
     direction_ = direction;
     direction_.normalize();
@@ -37,7 +37,8 @@ bool Parabaloid::is_within(Vector3d& point) const {
 }
 
 std::list<Intersection> Parabaloid::hit(Ray &ray) const {
-    std::list<double> ts = intersection(ray.origin(), ray.direction(), 1., 1., 0., 0., 0., 0., 0., 0., -r0_*r0_);
+    const Vector3d ray_origin = ray.origin();
+    std::list<double> ts = intersection(ray.origin(), ray.direction(), 1., 1., 0., 0., 0., 0., 0., 0., -r0_*r0_/pc);
 
     // No intersection - most common case
     if (ts.empty()) {
@@ -48,8 +49,8 @@ std::list<Intersection> Parabaloid::hit(Ray &ray) const {
     else if (ts.size() == 2) {
         double t1 = ts.front();
         double t2 = ts.back();
-        Vector3d point_in = ray.point(pc*std::min(t1, t2));
-        Vector3d point_out = ray.point(pc*std::max(t1, t2));
+        Vector3d point_in = ray.point(std::min(t1, t2));
+        Vector3d point_out = ray.point(std::max(t1, t2));
         return std::list<Intersection>{Intersection(ray, point_in, point_out)};
     }
     // One intersection - ignore it for now
