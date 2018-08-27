6ef2f7440a8cc9b86b83afdd211897d8202c7191
Ilya Pashchenko <in4pashchenko@gmail.com>
2018-08-28 00:09:39 +0300

    Prettify a bit ``integrate_tau_adaptive``

--- src/Observation.cpp
+++ src/Observation.cpp
@@ -27,29 +27,19 @@ void Observation::run(int n, double tau_max, double dt_max, double tau_min,
 	// Comment out for easy debug printing
 	#pragma omp parallel for schedule(dynamic) collapse(2)
 	// Actually, the best user-time:
-	// #pragma omp parallel for num_threads(4) schedule(dynamic) collapse(2)
+//	 #pragma omp parallel for num_threads(4) schedule(dynamic) collapse(2)
   for (unsigned long int j = 0; j < image_size.first; ++j) {
 	  // Don't need countr-jet side
 //      for (unsigned long int k = image_size.second/2-50; k < image_size.second; ++k) {
 	  for (unsigned long int k = 0; k < image_size.second; ++k) {
-//      unsigned long int n_pix = image_size.first*j + k + 1;
-//      std::cout << "Running on pixel " << j << "," << k << std::endl;
       auto &ray = rays[j*image_size.second+k];
       auto &pxl = pixels[j*image_size.second+k];
 
       auto ray_direction = ray.direction();
-//      std::cout << "In Observation.run hit with ray.origin = " << ray.origin()/pc;
       std::list<Intersection> list_intersect = jet->hit(ray);
       if (list_intersect.empty()) {
         continue;
       } else {
-
-
-//          double length_pc = list_intersect.front().length_pc();
-//          std::cout << "Path is " << length_pc << " for pixel " << j << ", " << k << std::endl;
-//          if (length_pc > 10) {
-//              std::cout << "Path is " << length_pc << " for pixel " << j << ", " << k << std::endl;
-//          }
 
 
 	      std::pair<double,double> tau_l_end;
@@ -57,12 +47,12 @@ void Observation::run(int n, double tau_max, double dt_max, double tau_min,
 	        tau_l_end = integrate_tau(list_intersect, ray_direction, nu, tau_max,
 	                                  n);
 	      } else if (integration_type == "adaptive") {
-//	      	  std::cout << "adaptive" << std::endl;
 		      tau_l_end = integrate_tau_adaptive(list_intersect, ray_direction, nu,
 		                                         tau_max, n, dt_max);
 	      }
 	      double background_tau = tau_l_end.first;
 	      double thickness = tau_l_end.second;
+
 
 	      // Write final values here inside integrate_i
 				state_type background_iquv{0., 0., 0., 0};
@@ -120,7 +110,7 @@ void Observation::run(int n, double tau_max, double dt_max, double tau_min,
 					pxl.setValue(value, thickness);
 	      }
       }
-          std::cout << "Finish on pixel " << j << "," << k << std::endl;
+//          std::cout << "Finish on pixel " << j << "," << k << std::endl;
       }
 
   }
@@ -217,19 +207,15 @@ Observation::integrate_tau_adaptive(std::list<Intersection> &list_intersect,
 	// Presicion for finding tau crossing
 	const double tau_precision = 1;
 
-
 	// Write final values in this variables inside for-cycle
 	double background_tau = 0.;
 	double thickness = 0.;
 
-	for (auto it = list_intersect.begin();
-	     it != list_intersect.end(); ++it) {
-		std::pair<double,double> tau_t_end;
+	for (auto it = list_intersect.begin(); it != list_intersect.end(); ++it) {
 		auto borders = (*it).get_path();
 
 		Vector3d point_in = borders.first;
 		Vector3d point_out = borders.second;
-
 
 		double length = (point_out - point_in).norm();
 		double dt = length/n;
@@ -243,69 +229,62 @@ Observation::integrate_tau_adaptive(std::list<Intersection> &list_intersect,
 		double optDepth = 0.0;
 		typedef runge_kutta_dopri5< double > stepper_type;
 		// Abs./Rel. error were -3, -14
-    auto stepper = make_dense_output(1E-5, 1E-14, dt_max,
-                                     stepper_type());
-//		using namespace std::placeholders;
+	    auto stepper = make_dense_output(1E-7, 1E-14, dt_max, stepper_type());
 		auto is_done = std::bind(check_opt_depth, tau_max, std::placeholders::_1);
-		auto ode_range = make_adaptive_range(std::ref(stepper), tau, optDepth,
-																				 0.0, length, dt);
-		auto found_iter = std::find_if(ode_range.first, ode_range.second,
-		                               is_done);
-		// tau_max somewhere on interval considered
+		auto ode_range = make_adaptive_range(std::ref(stepper), tau, optDepth, 0.0, length, dt);
+		auto found_iter = std::find_if(ode_range.first, ode_range.second, is_done);
+
+		// ``tau_max`` somewhere on interval considered
 		if (found_iter != ode_range.second) {
+		    double found_tau = found_iter.get_state();
+			// The dense out stepper now covers the interval where the condition changes
+            // Improve the solution by bisection
+            double t0 = stepper.previous_time();
+            double t1 = stepper.current_time();
+            double t_m;
+            double x_m = found_tau;
 
+            // Use odeint's resizing functionality to allocate memory for x_m
+            // Adjust_size_by_resizeability(x_m, optDepth, typename is_resizeable<double>::type());
+            while(std::abs(x_m - tau_max) > tau_precision) {
+                // Get the mid point time
+                t_m = 0.5 * (t0 + t1);
+                // Obtain the corresponding state
+                stepper.calc_state(t_m, x_m);
+                if (x_m > tau_max)
+                    // Condition changer lies before midpoint
+                    t1 = t_m;
+                else
+                    // Condition changer lies after midpoint
+                    t0 = t_m;
+            }
+            // We found the interval of size eps, take it's midpoint as final guess
+            t_m = 0.5 * (t0 + t1);
+            stepper.calc_state(t_m, x_m);
+            std::cout << "Found precise cross value tau = " << x_m << std::endl;
+            std::cout << "Originally it was tau = " << found_iter.get_state() << std::endl;
+            double t_tau_max = t_m;
+            // double t_tau_max = stepper.current_time();
 
-			double found_tau = found_iter.get_state();
-			// the dense out stepper now covers the interval where the condition changes
-	    // improve the solution by bisection
-	    double t0 = stepper.previous_time();
-//			std::cout << "Original t_0 = " << t0 << std::endl;
-	    double t1 = stepper.current_time();
-//			std::cout << "Original t_1 = " << t1 << std::endl;
-	    double t_m;
-	    double x_m = found_tau;
-	    // use odeint's resizing functionality to allocate memory for x_m
-//	    adjust_size_by_resizeability(x_m, optDepth, typename is_resizeable<double>::type());
-	    while(std::abs(x_m - tau_max) > tau_precision) {
-	        t_m = 0.5 * (t0 + t1);  // get the mid point time
-	        stepper.calc_state(t_m, x_m); // obtain the corresponding state
-//		      std::cout << "Current x_m = " << x_m << std::endl;
-	        if (x_m > tau_max)
-	            t1 = t_m;  // condition changer lies before midpoint
-	        else
-	            t0 = t_m;  // condition changer lies after midpoint
-	    }
-	    // we found the interval of size eps, take it's midpoint as final guess
-	    t_m = 0.5 * (t0 + t1);
-	    stepper.calc_state(t_m, x_m);
-			std::cout << "Found precise cross value tau = " << x_m << std::endl;
-			std::cout << "Originally it was tau = " << found_iter.get_state() << std::endl;
-			double t_tau_max = t_m;
-//			double t_tau_max = stepper.current_time();
+            Ray ray_tau_max(point_in, ray_direction);
+            Vector3d point_out_tau_max = ray_tau_max.point(t_tau_max);
+            // Set point of tau=tau_max as outer border for further integrations
+            it.operator*().set_point_out(point_out_tau_max);
 
-
-
-			Ray ray_tau_max(point_in, ray_direction);
-			Vector3d point_out_tau_max = ray_tau_max.point(t_tau_max);
-			// Set point of tau=tau_max as outer border for further integrations
-			it.operator*().set_point_out(point_out_tau_max);
-
-			// Delete all other furthest intersections if any
-			if (list_intersect.size() > 1) {
-				++it;
-				// This now is container.end()
-				it = list_intersect.erase(it, list_intersect.end());
-				// Move it before container.end() to make it end() on the new
-				// cycle
-				--it;
-			}
+            // Delete all other furthest intersections if any
+            if (list_intersect.size() > 1) {
+                ++it;
+                // This now is container.end()
+                it = list_intersect.erase(it, list_intersect.end());
+                // Move it before container.end() to make it end() on the new cycle
+                --it;
+            }
 		}
-		// Update background value (or write final values if this is last
-		// cycle)
-		background_tau += found_iter.get_state();
-		// FIXME: Here ``optDepth`` is 0 because out of scope or what? Not
-		// because of scope as ``length`` is nonzero.
-		thickness += length;
+        // Update background value (or write final values if this is last cycle)
+        background_tau += found_iter.get_state();
+        // FIXME: Here ``optDepth`` is 0 because out of scope or what? Not
+        // because of scope as ``length`` is nonzero.
+        thickness += length;
 	}
 	return std::make_pair(background_tau, thickness);
 }
